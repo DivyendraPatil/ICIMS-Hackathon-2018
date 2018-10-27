38fb409b524c61170b792220a995b500bee62de8
Divyendra Patil <divyendrapatil@gmail.com>
2018-10-27 16:51:09 -0400

    Add files via upload

---
+++ scraper.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# declare STRING variable
+
+Start="Script has started"
+echo $Start
+
+python3 google_images_download.py -k baked beans -f jpg -s large
+python3 google_images_download.py -k banana -f jpg -s large
+python3 google_images_download.py -k banana split -f jpg -s large
+python3 google_images_download.py -k cake -f jpg -s large
+python3 google_images_download.py -k blackcurrant -f jpg -s large
+python3 google_images_download.py -k blueberries -f jpg -s large
+python3 google_images_download.py -k boysenberry -f jpg -s large
+python3 google_images_download.py -k buffalo wings -f jpg -s large
+python3 google_images_download.py -k burritos -f jpg -s large
+python3 google_images_download.py -k cake -f jpg -s large
+python3 google_images_download.py -k cantaloupe -f jpg -s large
+python3 google_images_download.py -k cherimoya -f jpg -s large
+python3 google_images_download.py -k cherries -f jpg -s large
+python3 google_images_download.py -k chicofruit -f jpg -s large
+python3 google_images_download.py -k chocolate -f jpg -s large
+python3 google_images_download.py -k chocolate chip cookies -f jpg -s large
+python3 google_images_download.py -k corn  -f jpg -s large
+python3 google_images_download.py -k cranberries  -f jpg -s large
+python3 google_images_download.py -k cucumbers -f jpg -s large
+python3 google_images_download.py -k custardapple -f jpg -s large
+python3 google_images_download.py -k dates -f jpg -s large
+python3 google_images_download.py -k dragonfruit -f jpg -s large
+python3 google_images_download.py -k drain -f jpg -s large
+python3 google_images_download.py -k eggplant -f jpg -s large
+python3 google_images_download.py -k fig -f jpg -s large
+python3 google_images_download.py -k fortune cookies  -f jpg -s large
+python3 google_images_download.py -k fried cheese curd -f jpg -s large
+python3 google_images_download.py -k fried chicken -f jpg -s large
+python3 google_images_download.py -k gooseberry -f jpg -s large
+python3 google_images_download.py -k grapes -f jpg -s large
+python3 google_images_download.py -k guava  -f jpg -s large
+python3 google_images_download.py -k hot dogs -f jpg -s large
+python3 google_images_download.py -k hamburgers -f jpg -s large
+python3 google_images_download.py -k jackfruit -f jpg -s large
+python3 google_images_download.py -k jello -f jpg -s large
+python3 google_images_download.py -k kiwano -f jpg -s large
+python3 google_images_download.py -k kiwi -f jpg -s large
+python3 google_images_download.py -k kumquat -f jpg -s large
+python3 google_images_download.py -k lemon -f jpg -s large
+python3 google_images_download.py -k lime -f jpg -s large
+python3 google_images_download.py -k lychee -f jpg -s large
+python3 google_images_download.py -k macaroni and cheese -f jpg -s large
+python3 google_images_download.py -k mangoes  -f jpg -s large
+python3 google_images_download.py -k oranges -f jpg -s large
+python3 google_images_download.py -k papayas -f jpg -s large
+python3 google_images_download.py -k pea -f jpg -s large
+python3 google_images_download.py -k peaches -f jpg -s large
+python3 google_images_download.py -k pears -f jpg -s large
+python3 google_images_download.py -k pineapple  -f jpg -s large
+python3 google_images_download.py -k pizza -f jpg -s large
+python3 google_images_download.py -k pomegranate -f jpg -s large
+python3 google_images_download.py -k pop corn -f jpg -s large
+python3 google_images_download.py -k potato chips -f jpg -s large
+python3 google_images_download.py -k pumpkin -f jpg -s large
+python3 google_images_download.py -k raisins -f jpg -s large
+python3 google_images_download.py -k raspberries  -f jpg -s large
+python3 google_images_download.py -k raw oysters  -f jpg -s large
+python3 google_images_download.py -k salak -f jpg -s large
+python3 google_images_download.py -k soursop -f jpg -s large
+python3 google_images_download.py -k strawberry -f jpg -s large
+python3 google_images_download.py -k tacos  -f jpg -s large
+python3 google_images_download.py -k tamarind -f jpg -s large
+python3 google_images_download.py -k tomato -f jpg -s large
+python3 google_images_download.py -k waffle  -f jpg -s large
+python3 google_images_download.py -k watermelon -f jpg -s large
+
+EndScript="Script has ended"
+echo $EndScript
